83421a021bfa53d8c42e8dadcf54154d4946689f
ruzen42 <malledeferlive@gmail.com>
2025-10-28 19:31:59 +0500

    fix(remove warn)

--- Models/UserSettings/Settings.cs
+++ Models/UserSettings/Settings.cs
@@ -1,10 +1,8 @@
-using System.Text.Json.Serialization;
 using Avalonix.Models.UserSettings.AvalonixSettingsFiles;
 
 namespace Avalonix.Models.UserSettings;
 
 public record Settings
 {
-    [JsonIgnore] public Theme.Theme Theme { get; set; }
     public AvalonixSettings Avalonix { get; set; } = new ();
 }
