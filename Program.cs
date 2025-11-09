79e22aa321d8f472ecaf1bd032dc206f03e1d8e1
Nokskiy <145178078+Nokskiy@users.noreply.github.com>
2025-11-09 18:17:33 +0500

    Check bass.dll

--- Program.cs
+++ Program.cs
@@ -1,14 +1,27 @@
 ﻿#define DEBUG
 using Avalonia;
 using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace Avalonix;
 
 internal static class Program
 {
     [STAThread]
-    public static void Main(string[] args) =>
+    public static void Main(string[] args)
+    {
+        if (!Directory.GetFiles(Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory)!).Contains("bass.dll"))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Error: bass.dll not found!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadLine();
+            Environment.Exit(1);
+        }
         BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
 
     private static AppBuilder BuildAvaloniaApp() =>
         AppBuilder.Configure<App>().LogToTrace().UsePlatformDetect();
