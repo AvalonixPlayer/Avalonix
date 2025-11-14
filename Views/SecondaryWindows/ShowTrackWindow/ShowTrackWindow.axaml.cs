05257ff7f2f8cc5b53225d2eb7db57c283849757
Nokskiy <145178078+Nokskiy@users.noreply.github.com>
2025-11-14 22:29:43 +0500

    Fix visual

--- Views/SecondaryWindows/ShowTrackWindow/ShowTrackWindow.axaml.cs
+++ Views/SecondaryWindows/ShowTrackWindow/ShowTrackWindow.axaml.cs
@@ -13,33 +13,34 @@ namespace Avalonix.Views.SecondaryWindows.ShowTrackWindow;
 public partial class ShowTrackWindow : Window
 {
     private readonly ILogger<WindowManager> _logger;
+
     public ShowTrackWindow(ILogger<WindowManager> logger, Track track)
     {
         _logger = logger;
-        InitializeComponent();   
+        InitializeComponent();
         logger.LogInformation("ShowTrackWindow initialized");
         Title += track.Metadata.TrackName;
-        
+
         InitializeMainFields(track);
         InitializeAdditionalFields(track);
     }
 
     private void InitializeAdditionalFields(Track track)
     {
-        Album.Content += track.Metadata.Album;
-        Genre.Content += track.Metadata.Genre;
-        MediaFileFormat.Content += track.Metadata.MediaFileFormat;
-        Year.Content += track.Metadata.Year.ToString();
-        Duration.Content += ToHumanFriendlyString(track.Metadata.Duration);
+        Album.Content += " " + track.Metadata.Album;
+        Genre.Content += " " + track.Metadata.Genre;
+        MediaFileFormat.Content += " " + track.Metadata.MediaFileFormat;
+        Year.Content += " " + track.Metadata.Year;
+        Duration.Content += " " + ToHumanFriendlyString(track.Metadata.Duration);
     }
 
     private void InitializeMainFields(Track track)
     {
-        Name.Content += track.Metadata.TrackName;
-        Artist.Content += track.Metadata.Artist;
-        Lyrics.Text = track.Metadata.Lyric;
+        Name.Content += " " + track.Metadata.TrackName;
+        Artist.Content += " " + track.Metadata.Artist;
+        Lyrics.Text = " " + track.Metadata.Lyric;
     }
-    
+
     private static string ToHumanFriendlyString(TimeSpan timeSpan)
     {
         if (timeSpan.Hours > 0)
