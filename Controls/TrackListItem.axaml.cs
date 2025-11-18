19281408aea3708682d4d41e571c6235ceb99c5e
ruzen42- <malledeferlive@gmail.com>
2025-11-18 22:08:14 +0500

    fix shell.nix

---
+++ Controls/TrackListItem.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Avalonix.Controls;
+
+public partial class TrackListItem : ListBoxItem
+{
+    public TrackListItem()
+    {
+        InitializeComponent();
+    }
+}
+
