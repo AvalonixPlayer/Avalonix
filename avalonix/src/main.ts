ceaeb32141fb58244dc6ad4d556993aa0072025c
Nokskiy <145178078+Nokskiy@users.noreply.github.com>
2026-03-21 16:05:04 +0500

    fix

--- avalonix/src/main.ts
+++ avalonix/src/main.ts
@@ -1,12 +1,11 @@
-
-
-
-
 setInterval(() => {
+  setBG();
+}, 10);
+
+async function setBG() {
   let x = window.screenX /2 + window.innerWidth / 2;
   let y = window.screenY /2  + window.innerHeight /2;
   const col1 = getComputedStyle(document.documentElement).getPropertyValue("--background-gradient1");
   const col2 = getComputedStyle(document.documentElement).getPropertyValue("--background-gradient2");
 	document.body.style.background = `radial-gradient(circle 350px at ${x}px ${y}px, ${col1}, ${col2} )`;
-
-}, 10);
+}
