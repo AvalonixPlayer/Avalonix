012e970dd8c8345b8402e43b2c99148e0532eae7
Nokskiy <145178078+Nokskiy@users.noreply.github.com>
2026-04-27 21:08:08 +0500

    feat(tabs switching)

--- avalonix/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { invoke } from "@tauri-apps/api/core";
-
-invoke("get_tracks_ids")
-  .then((ids) => console.log("tracks ids: " + ids))
-  .catch((e) => console.error(e));
-
-invoke("get_albums_ids")
-  .then((ids) => console.log("albums ids: " + ids))
-  .catch((e) => console.error(e));
